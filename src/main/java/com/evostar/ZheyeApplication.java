84884ad353af73960933faeddd12bd02306df9bc
jundao <1242006911@qq.com>
2020-06-29 16:04:15 +0800

    stage1

---
+++ src/main/java/com/evostar/ZheyeApplication.java
@@ -0,0 +1,13 @@
+package com.evostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZheyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZheyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evostar/ZheyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZheyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
