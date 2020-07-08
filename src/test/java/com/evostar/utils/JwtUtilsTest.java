c72dc4f93f9e6badcccd7e411fdf3c109f490521
jundao <1242006911@qq.com>
2020-07-08 16:55:00 +0800

    G

--- src/test/java/com/evostar/utils/JwtUtilsTest.java
+++ src/test/java/com/evostar/utils/JwtUtilsTest.java
@@ -9,8 +9,9 @@ import java.util.Map;
 class JwtUtilsTest extends ApplicationTests {
     @Test
     public void getToken(){
-        String token = JwtUtils.createJWTToken(2,120);
+        //String token = JwtUtils.createJWTToken(2,120);
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTQyMDM0NTksInVzZXJJZCI6IjEiLCJ2ZXJzaW9uIjoiMS4wIiwiaWF0IjoxNTk0MTk2MjU5fQ.DvBrMXldQr-NDPHgbZffqfCdaxskJJ0OMr_jivpa19s";
         Map<String, Claim> claim = JwtUtils.verifyJWTToken(token);
-        System.out.println(claim.get("userId"));
+        System.out.println(claim.get("userId").asString());
     }
 }
