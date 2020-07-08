e1199a4c5d9c6771f7078ec8ac624e684077339d
jundao <1242006911@qq.com>
2020-07-08 17:08:15 +0800

    G

--- src/main/java/com/evostar/configuration/CorsFilter.java
+++ src/main/java/com/evostar/configuration/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        //response.setHeader("Access-Control-Allow-Credentials", "true");
         //response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
         chain.doFilter(req, res);
     }
